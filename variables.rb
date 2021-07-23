b82d5bfb783a08c786b33e8f2fb50a57918f009e
Alex McConnell <alexmmcconnell@gmail.com>
2021-07-23 13:49:08 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Emma Watson"
+is_active = true
+famous_person = "Daniel Radcliffe"
+num_3 = 5
+num_9 = 23
+sum = num_3 + num_9
+animals = ["zebra", "tiger", "owl"]
+animals.shift()
+p famous_person.size
