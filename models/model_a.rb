1ac5570ee6b90f41ab2e993905eab956ce27d18e
Alex McConnell <alexmmcconnell@gmail.com>
2021-07-23 13:53:30 -0600

    Add school information

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+SCHOOL = {
+  science_teacher: "Joe Brown",
+  math_teacher: "Bobby Sands",
+  number_of_kids: 150,
+  number_of_grades: 3,
+  is_middle_school: true,
+  is_high_school: false,
+  lunch_options: ["PBJ", "Chicken Sandwich", "Cheeseburger"],
+  after_school_activities: ["Board Game Club", "Drama Club", "Sports Club"]
+}
