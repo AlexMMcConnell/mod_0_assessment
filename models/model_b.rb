7d9563e96b7cbbfd18efd1f4a47f885d2d88bcb3
Alex McConnell <alexmmcconnell@gmail.com>
2021-07-23 13:56:13 -0600

    Add pet information

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+PET {
+  name: "Aliera",
+  breed: "Pug",
+  age: 8,
+  weight: 20.4,
+  is_hungry: false,
+  is_playful: true,
+  favorite_toys: ["Monkey", "Banana", "Bone"],
+  favorite_activities: ["Sleeping", "Playing", "Messing with my other dog"]
+}
